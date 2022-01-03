b87ec5049ad3aeb9af0838681c9e6929ac55c494
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-03 15:10:29 +0600

    products completed

--- assets/js/productslide.js
+++ assets/js/productslide.js
@@ -1,30 +1,120 @@
 $(document).ready(function() {
+    /** SOLID PIECE SLIDER START */
 
-    const sliderContent = $('.product-slider-for');
+    const solidPieceContent = $('.product-solidpiece-for');
+    const solidBackButton = '<button class="slick-prev slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg></button>';
+    const solidNextButton = '<button class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg></button>';
 
-    sliderContent.slick({
+    solidPieceContent.slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        arrows: false,
+        arrows: true,
         fade: false,
-        asNavFor: '.product-slider-nav'
+        asNavFor: '.product-solidpiece-nav',
+        prevArrow: solidBackButton,
+        nextArrow: solidNextButton,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    vertical: true
+                }
+            }
+        ]
     });
 
-    const sliderNav = $('.product-slider-nav');
-    const backButton = '<button class="slick-prev slick-arrow"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="25" r="25" transform="rotate(-90 25 25)" fill="#F8F8F9"/><path fill-rule="evenodd" clip-rule="evenodd" d="M17.6513 27.8038C17.3584 27.5109 17.3584 27.036 17.6513 26.7431L24.4695 19.9249C24.6101 19.7843 24.8009 19.7053 24.9998 19.7053C25.1987 19.7053 25.3895 19.7843 25.5302 19.9249L32.3483 26.7431C32.6412 27.036 32.6412 27.5109 32.3483 27.8038C32.0554 28.0967 31.5806 28.0967 31.2877 27.8038L24.9998 21.5159L18.712 27.8038C18.4191 28.0967 17.9442 28.0967 17.6513 27.8038Z" fill="#DFE3EA"/></svg></button>';
-    const nextButton = '<button class="slick-next slick-arrow"><svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="25" cy="25" r="25" transform="rotate(90 25 25)" fill="#F8F8F9"/><path fill-rule="evenodd" clip-rule="evenodd" d="M32.3487 22.1962C32.6416 22.4891 32.6416 22.964 32.3487 23.2569L25.5305 30.0751C25.3899 30.2157 25.1991 30.2947 25.0002 30.2947C24.8013 30.2947 24.6105 30.2157 24.4698 30.0751L17.6517 23.2569C17.3588 22.964 17.3588 22.4891 17.6517 22.1962C17.9446 21.9033 18.4194 21.9033 18.7123 22.1962L25.0002 28.4841L31.288 22.1962C31.5809 21.9033 32.0558 21.9033 32.3487 22.1962Z" fill="#DFE3EA"/></svg></button>';
+    const sliderSolidePieceNav = $('.product-solidpiece-nav');
     
-    sliderNav.slick({
+    sliderSolidePieceNav.slick({
         slidesToShow: 3,
         slidesToScroll: 1,
-        asNavFor: '.product-slider-for',
+        asNavFor: '.product-solidpiece-for',
         dots: false,
         arrows: false,
-        vertical: false,
+        vertical: true,
         centerMode: true,
-        focusOnSelect: true,
-        prevArrow: backButton,
-        nextArrow: nextButton
+        focusOnSelect: true
+    });
+
+    /** SOLID PIECE SLIDER END */
+
+    
+    /** THREE PIECE SLIDER START */
+
+    const threeSliderContent = $('.product-threepiece-for');
+    const threeBackButton = '<button class="slick-prev slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg></button>';
+    const threeNextButton = '<button class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg></button>';
+
+    threeSliderContent.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        asNavFor: '.product-threepiece-nav',
+        prevArrow: threeBackButton,
+        nextArrow: threeNextButton,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    vertical: true
+                }
+            }
+        ]
+    });
+
+    const threeSliderNav = $('.product-threepiece-nav');
+    
+    threeSliderNav.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.product-threepiece-for',
+        dots: false,
+        arrows: false,
+        vertical: true,
+        centerMode: true,
+        focusOnSelect: true
     });
     
+    /** THREE PIECE SLIDER END */
+
+    
+    /** ACCESSORIES SLIDER START */
+    
+    const accessoriesSliderContent = $('.product-accessories-for');
+    const accessoriesBackButton = '<button class="slick-prev slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/></svg></button>';
+    const accessoriesNextButton = '<button class="slick-next slick-arrow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/></svg></button>';
+
+    accessoriesSliderContent.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        asNavFor: '.product-accessories-nav',
+        prevArrow: accessoriesBackButton,
+        nextArrow: accessoriesNextButton,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    vertical: true
+                }
+            }
+        ]
+    });
+
+    const accessoriesSliderNav = $('.product-accessories-nav');
+    
+    accessoriesSliderNav.slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.product-accessories-for',
+        dots: false,
+        arrows: false,
+        vertical: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+    
+    /** ACCESSORIES SLIDER END */
 });
