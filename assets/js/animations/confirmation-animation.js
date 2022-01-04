9ded975e4f3f231dfbb7f02b48be9aed103bafbd
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-04 19:13:04 +0600

    order confirmation completed

---
+++ assets/js/animations/confirmation-animation.js
@@ -0,0 +1,14 @@
+
+TweenMax.from(".confirmation-details", 1, {
+    opacity: 0,
+    delay: 1.25,
+    y: 20,
+    ease: Expo.easeInOut
+});
+
+TweenMax.from(".orderlist", 2, {
+    opacity: 0,
+    delay: 1.25,
+    y: 20,
+    ease: Expo.easeInOut
+});
