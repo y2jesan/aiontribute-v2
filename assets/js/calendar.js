232df45eea6f5b8bd2f5a5ff4e8524621d03ec12
it27472 <istiak.tushar.qtec@gmail.com>
2022-01-04 15:23:25 +0600

    create page completed

---
+++ assets/js/calendar.js
@@ -0,0 +1,9 @@
+const birthDayPick = MCDatepicker.create({ 
+    el: '#birthdate',
+    dateFormat: 'MM-DD-YYYY'
+});
+
+const deathDayPick = MCDatepicker.create({ 
+    el: '#deathdate',
+    dateFormat: 'MM-DD-YYYY'
+});
